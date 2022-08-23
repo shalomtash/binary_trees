1c7592c2be9bf99600830cfe346cdce0c2027611
Shalomtash <99330267+shalomtash@users.noreply.github.com>
2022-08-23 12:19:11 +0300

    Checks if a given node is a root

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_is_root - Checks if a given node is a root.
+ * binary_tree_is_root - Checks if a given node is a root
  *
  * @node: Pointer to a node of the tree.
  *
