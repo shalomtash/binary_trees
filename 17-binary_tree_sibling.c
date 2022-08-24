9129702bf6f6d7a2159835864a1268651171abb4
shalomtash <shalomtash93@gmail.com>
2022-08-24 15:43:36 +0000

    a function that finds a sibling of a node

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,10 +10,10 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (!node || !(node->parent))
+	if (!node || !node->parent)
 		return (NULL);
-
-	if (node == node->parent->left)
+	if (node->parent->left == node)
 		return (node->parent->right);
-	return (node->parent->left);
+	else
+		return (node->parent->left);
 }	
