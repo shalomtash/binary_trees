4b8937157b5fb84a2bec2a7f2d2e7064c3b2228e
shalomtash <shalomtash93@gmail.com>
2022-08-25 18:59:11 +0000

    a function that inserts a value in a binary search tree

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,57 +1,51 @@
 #include "binary_trees.h"
 
 /**
- * bst_in - checks if node is inserted
- *
- * @tree: tree root
- * @value: node value
- * Return: pointer to the new node
- */
-bst_t *bst_in(bst_t **tree, int value)
-{
-	if (value < (*tree)->n)
-	{
-		if ((*tree)->left == NULL)
-		{
-			(*tree)->left = binary_tree_node(*tree, value);
-			return ((*tree)->left);
-		}
-		else
-		{
-			return (bst_in(&((*tree)->left), value));
-		}
-	}
-
-	if (value > (*tree)->n)
-	{
-		if ((*tree)->right == NULL)
-		{
-			(*tree)->right = binary_tree_node(*tree, value);
-			return ((*tree)->right);
-		}
-		else
-		{
-			return (bst_in(&((*tree)->right), value));
-		}
-	}
-
-	return (NULL);
-}
-
-/**
  * bst_insert - inserts a value in a Binary Search Tree
+ * @tree: double pointer to the root node of the BST to insert the value
+ * @value: value to store in the node to be inserted
  *
- * @tree: tree root
- * @value: node value
- * Return: pointer to the new node
+ * Description: If the address stored in tree is NULL, the created node must
+ * become the root node. If the value is already present in the tree, it must
+ * be ignored
+ *
+ * Return: a pointer to the created node, or NULL on failure
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-	if (*tree == NULL)
-	{
-		*tree = binary_tree_node(NULL, value);
-		return (*tree);
-	}
+	bst_t *tmp;
 
-	return (bst_in(tree, value));
+	if (tree)
+	{
+		if (*tree == NULL)
+		{
+			*tree = (bst_t *)binary_tree_node(NULL, value);
+			return (*tree);
+		}
+		tmp = *tree;
+		while (tmp)
+		{
+			if (tmp->n == value)
+				break;
+			if (tmp->n > value)
+			{
+				if (!tmp->left)
+				{
+					tmp->left = (bst_t *)binary_tree_node(tmp, value);
+					return (tmp->left);
+				}
+				tmp = tmp->left;
+			}
+			else if (tmp->n < value)
+			{
+				if (!tmp->right)
+				{
+					tmp->right = (bst_t *)binary_tree_node(tmp, value);
+					return (tmp->right);
+				}
+				tmp = tmp->right;
+			}
+		}
+	}
+	return (NULL);
 }
